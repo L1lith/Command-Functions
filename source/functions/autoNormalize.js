ac5c406ad3e8ff0b0626fdeb8a8ba9349a235bc7
L1lith <lily@l1lith.com>
2022-04-29 02:52:44 -0600

    fix a bug with autoNormalize

--- source/functions/autoNormalize.js
+++ source/functions/autoNormalize.js
@@ -7,17 +7,19 @@ const falsy = ['false', 'no', 'n']
 function autoNormalize(value, formatDetails) {
   // Automatically helps us format our inputs correctly
   const { format, options } = formatDetails
-  if (format === Boolean) {
-    value = stripString(value)
-    if (truthy.includes(value)) return true
-    if (falsy.includes(value)) return false
-  } else if (format === String) {
+  if (format === Boolean && typeof value == 'string') {
+    if (typeof value == 'string') {
+      const newValue = stripString(value)
+      if (truthy.includes(newValue)) return true
+      if (falsy.includes(newValue)) return false
+    }
+  } else if (format === String && typeof value == 'string') {
     if (options.lowercase === true) value = value.toLowerCase()
     if (options.trimmed === true) value = value.trim()
     if (options.uppercase === true) value = value.toUpperCase()
   } else if (format === Number) {
-    value = parseFloat(value)
-    if (!isFinite(value) || isNaN(value)) return value
+    const newValue = parseFloat(value)
+    if (isFinite(newValue)) value = newValue
     if (options.hasOwnProperty('min')) {
       value = Math.max(options.min, value)
     }
