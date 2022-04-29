fa17a0b35b43694aead9e0a0ef5a0e20041c74ac
L1lith <lily@l1lith.com>
2022-04-29 01:02:02 -0600

    update the project

--- .babelrc.js
+++ .babelrc.js
@@ -1,6 +1,16 @@
 module.exports = {
-  presets: [['@babel/preset-env', { modules: 'cjs' }]],
-  plugins: ['@babel/plugin-transform-runtime', 'add-module-exports']
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: 'cjs',
+        targets: {
+          node: 14
+        }
+      }
+    ]
+  ],
+  plugins: ['add-module-exports']
   //   env: {
   //     test: {
   //       // Make jest not angry when we use fancy features, not a problem for users because they'll be bundling our lib anyways
