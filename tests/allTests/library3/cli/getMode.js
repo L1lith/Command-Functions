3cb08af6bc465b03bbcdfe6f49937ba700fe490c
L1lith <lily@l1lith.com>
2022-05-22 18:27:42 -0600

    add some cli mode testing

---
+++ tests/allTests/library3/cli/getMode.js
@@ -0,0 +1,23 @@
+const chai = require('chai')
+const chaiExec = require('@jsdevtools/chai-exec')
+const { expect } = chai
+const strip = require('strip-color')
+
+chai.use(chaiExec)
+chai.use(require('chai-match'))
+const cliStringRegex = /cli/
+
+describe('the getMode command in CLI', () => {
+  it('should return the current mode', () => {
+    // Run your CLI
+    const myCLI = chaiExec('node tests/library3/getMode')
+    expect(myCLI).to.exit.with.code(0)
+    expect(strip(myCLI.stdout)).to.match(cliStringRegex)
+  })
+
+  it('can run silently', () => {
+    const myCLI = chaiExec('node tests/library3/getMode --silent')
+    expect(myCLI).to.exit.with.code(0)
+    expect(myCLI).stdout.to.equal('')
+  })
+})
