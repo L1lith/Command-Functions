e0c3b68c31acd1e3c6ebb3b53764b8f14f55b397
L1lith <lily@l1lith.com>
2022-05-22 18:43:41 -0600

    fix getmode

--- tests/allTests/library3/exports/getMode.js
+++ tests/allTests/library3/exports/getMode.js
@@ -3,11 +3,11 @@ const { expect } = chai
 
 describe('the getMode function is exported correctly', () => {
   let getMode = null
-  it('can load the getTime function', () => {
+  it('can load the getMode function', () => {
     getMode = require('../../../library3/getMode')
     expect(getMode).to.be.a('function')
   })
-  it('the getTime function returns a date', () => {
+  it('the getMode function returns node', () => {
     const mode = getMode()
     expect(mode).to.equal('node')
   })
